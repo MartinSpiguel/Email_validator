0fde2d92fc8a0238f0e1f4b371480b363f129227
Martin Spiguel <maspiguel@gmail.com>
2023-06-03 00:46:08 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple email validator made with Haskell.
---
+++ validator.hs
@@ -0,0 +1,23 @@
+domains = ["gmail.com", "yahoo.com", "hotmail.com", "outlook.com", "icloud.com", 
+           "aol.com", "mail.com", "yandex.com", "zoho.com", "protonmail.com", 
+           "gmx.com", "live.com", "me.com", "inbox.com", "rocketmail.com"]
+
+-- Validates the email
+validator :: String -> Bool
+validator mail | not (hasAt mail) || not (hasValidDomain mail domains) = False
+               | otherwise = True
+
+-- Checks if mail has an at
+hasAt :: String -> Bool
+hasAt [] = False
+hasAt (x:xs) = x == '@' || hasAt xs
+
+-- Checks if mail has a known domain
+hasValidDomain :: String -> [String] -> Bool
+hasValidDomain _ [] = False
+hasValidDomain mail (dom:doms) = getDomain mail == dom || hasValidDomain mail doms
+
+-- Takes a mail and returns the domain
+getDomain :: String -> String
+getDomain (x:xs) | x /= '@' = getDomain xs
+                 | otherwise = xs
